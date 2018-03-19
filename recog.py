69470ce0d5b263e09307606aa9e4ecb3261bdc6d
Sumit Saha <sumit.saha666@gmail.com>
2018-03-19 23:00:36 +0530

    Update recog.py

--- recog.py
+++ recog.py
@@ -8,19 +8,28 @@ class face_recog:
                 pass
         
         def get_images(self):
+        
         	for file in glob.glob("*.jpg"):
 		        self.faces.append(file)
 		print(self.faces)	
 	
-        def recog_image(self):
-        	known_image = face_recognition.load_image_file("test-9.jpeg")
-        	unknown_image = face_recognition.load_image_file("test-8.jpeg")
+	def recog_image(self,file1,file2):
+        	
+        	known_image = face_recognition.load_image_file("{}".format(file1))
+        	unknown_image = face_recognition.load_image_file("{}".format(file2))
              
         	biden_encoding = face_recognition.face_encodings(known_image)[0]	
         	unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
         	results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-        	print(results)
+        	
+        	return(results)
         	
 if __name__ == '__main__':
         fr = face_recog()
-        fr.get_images()
+	fr.get_images()
+	for file1 in fr.faces:
+		for file2 in fr.faces:
+			if(fr.recog_image(file1,file2)==True):
+				print("{} and {} are same".format(file1,file2))
+			else:
+				print("{} and {} are different".format(file1,file2))
