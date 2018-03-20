4ab4452ac0cd036e12d76047aa6bc8d852e1993b
Sumit Saha <sumit.saha666@gmail.com>
2018-03-20 20:09:28 +0530

    Add files via upload

---
+++ automated-beta.py
@@ -0,0 +1,88 @@
+import numpy as np
+import cv2
+import glob
+import face_recognition
+
+class face_recog:
+	
+	faces = []
+	
+	def __init__(self):
+		self.get_files()
+		for file in self.faces:
+			print(file)
+
+	def get_files(self):
+		for file in glob.glob('*.jpg'):
+			self.faces.append(file)	
+	
+	def clahe_histo(self,file_name):
+		file_name = file_name.strip('.jpg')
+		img = cv2.imread('{}.jpg'.format(file_name),0) 
+		clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+		cl1 = clahe.apply(img)
+		cv2.imwrite('histo-result-{}.png'.format(file_name),cl1)
+	
+	# need to have a automated placeholder module to scout haarcascade files
+	def haar_cascade(self,file_name):
+		file_name = file_name.strip('.jpg')
+		print(file_name)
+		face_cascade = cv2.CascadeClassifier('/home/sumitsaha/opencv-3.3.1/data/haarcascades/haarcascade_frontalface_default.xml')
+		eye_cascade = cv2.CascadeClassifier('/home/sumitsaha/opencv-3.3.1/data/haarcascades/haarcascade_eye.xml')
+		img = cv2.imread('{}.jpg'.format(file_name))
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+		for (x,y,w,h) in faces:
+		    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+		    roi_gray = gray[y:y+h, x:x+w]
+		    roi_color = img[y:y+h, x:x+w]
+		    eyes = eye_cascade.detectMultiScale(roi_gray)
+		    for (ex,ey,ew,eh) in eyes:
+		        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		cv2.imwrite('haar-cascade-{}.png'.format(file_name),img)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+	
+	def canny_edge(self,file_name):
+		file_name = file_name.strip('.jpg')
+		img = cv2.imread('{}.jpg'.format(file_name),cv2.IMREAD_GRAYSCALE)
+	 
+		filter = cv2.Canny(img,100,200)
+	 
+		#cv2.imshow('Original - Canny Edge',img)
+		cv2.imwrite('canny-edge-{}.png'.format(file_name),filter)
+		
+	def recog_image(self,file1,file2):
+		known_image = face_recognition.load_image_file("{}".format(file1))
+		unknown_image = face_recognition.load_image_file("{}".format(file2))      
+		       
+		biden_encoding = face_recognition.face_encodings(known_image)[0]	
+		unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+		
+		results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+		return(results)
+	
+	def enchance_images(self):
+		for file in self.faces:
+			self.clahe_histo(file)
+		for file in self..faces:
+			self.canny_edge(file)
+		for file in self.faces:
+			self.haar_cascade(file) 
+		
+	def compare_faces(self):	
+		for file1 in self.faces:
+			for file2 in self.faces:
+				if(file1!=file2):
+					if(self.recog_image(file1,file2)==[True]):
+						print("{} and {} are same".format(file1,file2))
+					else:
+						print("{} and {} are different".format(file1,file2))
+		
+	 
+if __name__=='__main__':
+	
+	recognition = face_recog()
+	
+	recognition.enchance_images()
+	recognition.compare_faces()
